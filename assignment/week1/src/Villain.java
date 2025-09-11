51885e2340c36f24f86085790b95ea2f658d854f
kmi0817 <kmi0817@naver.com>
2025-09-12 01:07:13 +0900

    [feat] hp가 0보다 작아도 목숨이 남아 있다면 되살리고, 아니면 죽은 것으로 처리하는 로직 추가

--- assignment/week1/src/Villain.java
+++ assignment/week1/src/Villain.java
@@ -25,6 +25,19 @@ class Villain extends Fighter {
 
             super.attack(target);
             attackCount++;
+
+            Hero hero = (Hero) target;
+
+            if (hero.getHp() <= 0) {
+                hero.decrementLife();
+
+                if (hero.canRespawn()) {
+                    hero.setHp(hero.getMaxHp() + hero.getHp());
+                    System.out.printf("\t=> %s이/가 다시 살아났습니다 (목숨: %d, hp: %d).\n", hero.getName(), hero.getLives(), hero.getHp());
+                } else {
+                    System.out.println("\t=> " + hero.getName() + "은/는 다시 살아날 목숨이 없어 죽었습니다.");
+                }
+            }
         } catch (RuntimeException e) {
             System.out.println(e.getMessage());
         }
