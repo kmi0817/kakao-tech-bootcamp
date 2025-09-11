d1a1ec637b4ac462e9e3dc33c71e50b653dbc84f
kmi0817 <kmi0817@naver.com>
2025-09-11 17:17:02 +0900

    [feat] 환생시킬 히어로의 죽음 여부 확인 및 환생시킨 히어로의 hp 100으로 설정

--- assignment/week1/src/Healer.java
+++ assignment/week1/src/Healer.java
@@ -5,10 +5,16 @@ class Healer extends Hero {
     }
 
     void reviveHero(Hero hero) {
+        if (!hero.isDead()) {
+            throw new RuntimeException("히어로가 이미 살아 있으므로 환생시킬 수 없습니다.");
+        }
+
         if (getLives() < 2) {
             throw new RuntimeException("목숨을 나눠줄 수 없습니다.");
         }
 
+
+        hero.setHp(100);
         hero.incrementLife();
         decrementLife();
     }
