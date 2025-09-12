92dc00ccaba4e0233b5cd0988188c018383e46f9
kmi0817 <kmi0817@naver.com>
2025-09-13 01:06:45 +0900

    [feat] attack 메서드 안에서 synchronized statement 사용해 스레드 간 공격 동기화
    Hero의 hp가 다시 살아났을 때 출력되는 두 해설 사이에 다른 스레드의 해설이 들어와 있거나, 먼저 공격한 스레드보다 늦게 공격한 스레드의 공격 해설이 더 빨리 출력되어 hp가 10 -> 20처럼 순서가 엉키는 경우가 있었다. 이것들을 해결해줌.

--- assignment/week1/src/Fighter.java
+++ assignment/week1/src/Fighter.java
@@ -34,24 +34,26 @@ class Fighter extends Character{
     }
 
     void attack(Fighter target) {
-        try {
-            if (isDead()) {
-                throw new RuntimeException(getName() + "은/는 죽었기 때문에 다른 캐릭터를 공격할 수 없습니다.");
-            }
+        synchronized (target) {
+            try {
+                if (isDead()) {
+                    throw new RuntimeException(getName() + "은/는 죽었기 때문에 다른 캐릭터를 공격할 수 없습니다.");
+                }
 
-            if (target.isDead()) {
-                throw new RuntimeException(target.getName() + "은/는 이미 죽은 캐릭터라 공격할 수 없습니다.");
-            }
+                if (target.isDead()) {
+                    throw new RuntimeException(target.getName() + "은/는 이미 죽은 캐릭터라 공격할 수 없습니다.");
+                }
 
-            target.setHp(target.getHp() - power);
+                target.setHp(target.getHp() - power);
 
-            if (target.isDead()) {
-                System.out.printf("%s이/가 %d만큼 공격하여 %s이/가 죽었습니다.\n", getName(), getPower(), target.getName());
-            } else {
-                System.out.printf("%s이/가 %d만큼 공격하여 %s의 hp가 %d로 감소했습니다.\n", getName(), getPower(), target.getName(), target.getHp());
+                if (target.isDead()) {
+                    System.out.printf("%s이/가 %d만큼 공격하여 %s이/가 죽었습니다.\n", getName(), getPower(), target.getName());
+                } else {
+                    System.out.printf("%s이/가 %d만큼 공격하여 %s의 hp가 %d로 감소했습니다.\n", getName(), getPower(), target.getName(), target.getHp());
+                }
+            } catch (RuntimeException e) {
+                System.out.println(e.getMessage());
             }
-        } catch (RuntimeException e) {
-            System.out.println(e.getMessage());
         }
     }
 }
--- assignment/week1/src/Villain.java
+++ assignment/week1/src/Villain.java
@@ -18,28 +18,30 @@ class Villain extends Fighter {
 
     @Override
     void attack(Fighter target) {
-        try {
-            if (attackCount >= attackLimit) {
-                throw new RuntimeException(getName() + "은/는 공격 가능 횟수를 모두 소진해 더 이상 공격할 수 없습니다.");
-            }
-
-            super.attack(target);
-            attackCount++;
-
-            Hero hero = (Hero) target;
-
-            if (hero.getHp() <= 0) {
-                hero.decrementLife();
-
-                if (hero.canRespawn()) {
-                    hero.setHp(hero.getMaxHp() + hero.getHp());
-                    System.out.printf("\t=> %s이/가 다시 살아났습니다 (목숨: %d, hp: %d).\n", hero.getName(), hero.getLives(), hero.getHp());
-                } else {
-                    System.out.println("\t=> " + hero.getName() + "은/는 다시 살아날 목숨이 없어 죽었습니다.");
+        synchronized (target) {
+            try {
+                if (attackCount >= attackLimit) {
+                    throw new RuntimeException(getName() + "은/는 공격 가능 횟수를 모두 소진해 더 이상 공격할 수 없습니다.");
                 }
+
+                super.attack(target);
+                attackCount++;
+
+                Hero hero = (Hero) target;
+
+                if (hero.getHp() <= 0) {
+                    hero.decrementLife();
+
+                    if (hero.canRespawn()) {
+                        hero.setHp(hero.getMaxHp() + hero.getHp());
+                        System.out.printf("\t=> %s이/가 다시 살아났습니다 (목숨: %d, hp: %d).\n", hero.getName(), hero.getLives(), hero.getHp());
+                    } else {
+                        System.out.println("\t=> " + hero.getName() + "은/는 다시 살아날 목숨이 없어 죽었습니다.");
+                    }
+                }
+            } catch (RuntimeException e) {
+                System.out.println(e.getMessage());
             }
-        } catch (RuntimeException e) {
-            System.out.println(e.getMessage());
         }
     }
 }
